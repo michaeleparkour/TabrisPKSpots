a0faff5771356647543fab32f035cff9483b0a8d
michaeleparkour <michaeleparkour@gmail.com>
2016-04-08 21:06:10 +0300

    trying mapbox plugin

--- cordova/hooks/api_key_hook.js
+++ cordova/hooks/api_key_hook.js
@@ -1,22 +1,15 @@
 module.exports = function(context) {
-    var cordova_util = context.requireCordovaModule("cordova-lib/src/cordova/util");
-    var ConfigParser = context.requireCordovaModule("cordova-lib/src/configparser/ConfigParser");
-    var projectRoot = cordova_util.isCordova();
-    var xml = cordova_util.projectConfig(projectRoot);
-    var cfg = new ConfigParser(xml);
 
-    cfg.doc
-        .findall("plugin/[@name='cordova-plugin-mapbox']")[0]
-        .findall("variable/[@name='ACCESS_TOKEN']")[0]
-        .set("value", process.env.ACCESS_TOKEN);
+    var pluginlist = ["cordova-plugin-mapbox --variable ACCESS_TOKEN="+process.env.ACCESS_TOKEN];
 
-    console.log(
-        "Setting ACCESS_TOKEN to: " +
-        cfg.doc
-            .findall("plugin/[@name='cordova-plugin-mapbox']")[0]
-            .findall("variable/[@name='ACCESS_TOKEN']")[0]
-            .get("value")
-    );
+    var sys = require('sys');
+    var exec = require('child_process').exec;
 
-    cfg.write();
+    function puts(error, stdout, stderr) {
+        sys.puts(stdout)
+    }
+
+    pluginlist.forEach(function(plug) {
+        exec("cordova plugin add " + plug, puts);
+    });
 };
